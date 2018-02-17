ff2566a53d5b608de67b2a73399f04c04d25055b
vickydasta <vicky.vernando@student.unri.ac.id>
2018-02-17 05:39:51 +0000

    add argument server

--- approx.py
+++ approx.py
@@ -3,6 +3,10 @@ from sklearn.datasets import load_iris
 import numpy as np 
 import matplotlib.pyplot as plt
 
+import sys
+
+server = None
+
 net = Neural()
 
 x = np.linspace(0, 4, 1000)
@@ -20,6 +24,9 @@ net.init_weight()
 net.train(x, y, 200)
 net.accuracy()
 
-plt.plot(y)
-plt.plot([net.predict(i) for i in x_test])
-plt.show()
+cmd = sys.argv[1:]
+if cmd:
+    if cmd[0] == 'server' or 'serve':
+        plt.plot(y)
+        plt.plot([net.predict(i) for i in x_test])
+        plt.show()
